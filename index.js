70f1c0b31a73943f7e28a7b84854c2fddf4ef3ee
souvikkppp <103935378+souvikkppp@users.noreply.github.com>
2022-09-25 10:39:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+{/*here we need <App speace /> to give space part of jsx style, if here a child element  then <App>fghgh</App>*/}
